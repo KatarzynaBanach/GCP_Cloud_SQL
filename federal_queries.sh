0d55e6f776a41934afe8a8f54aaafdc51f6e6dbb
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-01 23:21:21 +0200

    Update federal_queries.sh

--- federal_queries.sh
+++ federal_queries.sh
@@ -2,9 +2,14 @@ INSTANCE_NAME=names-inst-new
 PASSWORD=passw
 CONNECTION_ID=$(gcloud sql instances describe $INSTANCE_NAME  --format="value(connectionName)")
 PROJECT_ID=$(gcloud info --format='value(config.project)')
-
+PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
+CONNECTION_SERVICE_ACCOUNT=service-$PROJECT_NUMBER@gcp-sa-bigqueryconnection.iam.gserviceaccount.com
 
 bq mk --connection --display_name='Cloud SQL connection - Names' --connection_type='CLOUD_SQL' \
   --properties="{\"instanceId\":\"$CONNECTION_ID\",\"database\":\"bts\",\"type\":\"MYSQL\"}" \
   --connection_credential="{\"username\":\"root\", \"password\":\"$PASSWORD\"}" \
   --project_id=$PROJECT_ID --location=eu names_connection_id
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member=serviceAccount:$CONNECTION_SERVICE_ACCOUNT \
+    --role=roles/cloudsql.client
