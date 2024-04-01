0e0e1308f7e59a759380b6ac6776a6b1f7432554
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-01 23:35:05 +0200

    Update federated_queries.sh

--- federated_queries.sh
+++ federated_queries.sh
@@ -5,13 +5,16 @@ PROJECT_ID=$(gcloud info --format='value(config.project)')
 PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
 CONNECTION_SERVICE_ACCOUNT=service-$PROJECT_NUMBER@gcp-sa-bigqueryconnection.iam.gserviceaccount.com
 
+# Enable BigQuery API.
 gcloud services enable bigquery.googleapis.com
 
+# Create External Connetion to the Database.
 bq mk --connection --display_name='Cloud SQL connection - Names' --connection_type='CLOUD_SQL' \
   --properties="{\"instanceId\":\"$CONNECTION_ID\",\"database\":\"bts\",\"type\":\"MYSQL\"}" \
   --connection_credential="{\"username\":\"root\", \"password\":\"$PASSWORD\"}" \
   --project_id=$PROJECT_ID --location=eu names_connection_id
 
+# Add the Cloud SQL Client role to a service account relevant to External Connetion.
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member=serviceAccount:$CONNECTION_SERVICE_ACCOUNT \
     --role=roles/cloudsql.client
