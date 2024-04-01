90d7cb0a42d86bb2bb6c5b6c37f1fce82a12f1b7
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-01 18:28:11 +0200

    Update init_settings.sh

--- init_settings.sh
+++ init_settings.sh
@@ -3,7 +3,14 @@ PROJECT_ID=$(gcloud info --format='value(config.project)')
 INSTANCE_NAME=names-inst-new
 PASSWORD=passw
 
-QUERY="create database if not exists bts;
+gcloud sql instances create $INSTANCE_NAME --tier=db-n1-standard-1 --activation-policy=ALWAYS --region=us-central1
+gcloud sql users set-password root --host % --instance $INSTANCE_NAME --password $PASSWORD
+
+ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
+gcloud sql instances patch $INSTANCE_NAME --authorized-networks $ADDRESS
+
+MYSQLIP=$(gcloud sql instances describe $INSTANCE_NAME --format="value(ipAddresses.ipAddress)")
+mysql --host=$MYSQLIP --user=root --password --local-infile -e "create database if not exists bts;
 use bts;
 drop table if exists names;
 create table names (
@@ -15,12 +22,3 @@ FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (name,surname,age);"
-
-gcloud sql instances create $INSTANCE_NAME --tier=db-n1-standard-1 --activation-policy=ALWAYS --region=us-central1
-gcloud sql users set-password root --host % --instance $INSTANCE_NAME --password $PASSWORD
-
-ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
-gcloud sql instances patch $INSTANCE_NAME --authorized-networks $ADDRESS
-
-MYSQLIP=$(gcloud sql instances describe $INSTANCE_NAME --format="value(ipAddresses.ipAddress)")
-mysql --host=$MYSQLIP --user=root --password --local-infile -e $QUERY
