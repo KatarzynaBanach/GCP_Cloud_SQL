19559bff4b95395ba6eef0b01ee5abd4c9a865d2
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-03 23:13:57 +0200

    Update to_cloudstorage_to_bq.sh

--- to_cloudstorage_to_bq.sh
+++ to_cloudstorage_to_bq.sh
@@ -5,12 +5,16 @@ FILE_NAME='names.csv'
 DATASET_ID='names_dataset'
 TABLE_NAME='names'
 
+# Add the Storage Object Admin role to a service account relevant to Cloud SQL Instance.
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member=serviceAccount:$CONNECTION_SERVICE_ACCOUNT \
     --role roles/storage.objectAdmin
 
+# Create bucket.
 gcloud storage buckets create gs://$BUCKET_NAME --location=europe-central2
 
+
+# Export data into bucket.
 gcloud sql export csv names-inst-new gs://$BUCKET_NAME/$FILE_NAME \
 --database=bts \
 --offload \
@@ -18,17 +22,20 @@ gcloud sql export csv names-inst-new gs://$BUCKET_NAME/$FILE_NAME \
 --fields-terminated-by="2C" \
 --lines-terminated-by="0A"
 
+# Create Dataset.
 bq --location='EU' mk \
     --dataset \
     --description="Data from Cloud SQL" \
     --label=category:prod \
     $PROJECT_ID:$DATASET_ID
 
+# Create Table.
 bq mk \
 --table \
 $PROJECT_ID:$DATASET_ID.$TABLE_NAME \
 names_schema.json
 
+# Load data into table??.
 bq load \
 --source_format=CSV \
 $DATASET_ID.$TABLE_NAME \
