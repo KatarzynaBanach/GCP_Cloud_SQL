92397b44ab612d895a2dec0f3089ead12d78677c
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-04 20:29:02 +0200

    Update init_settings.sh

--- init_settings.sh
+++ init_settings.sh
@@ -1,13 +1,13 @@
 gcloud services enable sqladmin.googleapis.com
 PROJECT_ID=$(gcloud info --format='value(config.project)')  # Project ID for active project.
-INSTANCE_NAME=names-inst-new
+INSTANCE_NAME=netflix-sql
 PASSWORD=passw
 
 # Create Instance of MySQL.
 # The activation policy determines whether the instance remains active or is deactivated when there are no connections to it. 
 # Setting it to ALWAYS means the instance remains active even if there are no connections.
 #  % is a wildcard that means any host, indicating that the password can be used from any host.
-gcloud sql instances create $INSTANCE_NAME --tier=db-n1-standard-1 --activation-policy=ALWAYS --region=eu-central1 --database-version=MYSQL_8_0  
+gcloud sql instances create $INSTANCE_NAME --tier=db-n1-standard-1  --activation-policy=ALWAYS --region=europe-central2 --database-version=MYSQL_8_0  
 gcloud sql users set-password root --host % --instance $INSTANCE_NAME --password $PASSWORD
 
 
@@ -19,18 +19,22 @@ gcloud sql instances patch $INSTANCE_NAME --authorized-networks $ADDRESS
 # LOAD DATA LOCAL INFILE is used when the file is located on the client's filesystem and can be accessed locally by the MySQL client.
 # The version with LOAD DATA INFILE won't work it the case since it is used when the file is located on the server's filesystem 
 # or accessible via a network file path. However, MySQL does not directly support loading files from remote URLs or Cloud Storage locations.
-MYSQLIP=$(gcloud sql instances describe $INSTANCE_NAME --format="value(ipAddresses.ipAddress)")
-mysql --host=$MYSQLIP --user=root --password --local-infile -e "create database if not exists bts;
-use bts;
-drop table if exists names;
-create table names (
-name VARCHAR(10),
-surname VARCHAR(15),
-age INT);
-LOAD DATA LOCAL INFILE 'names_data.csv' INTO TABLE names
-FIELDS TERMINATED BY ','
+MYSQLIP=$(gcloud sql instances describe $INSTANCE_NAME --format='value(ipAddresses.ipAddress)')
+mysql --host=$MYSQLIP --user=root --password --local-infile -e "CREATE DATABASE IF NOT EXISTS netflix;
+USE netflix;
+DROP TABLE IF EXISTS netflix_shows;
+CREATE TABLE netflix_shows (
+viewer_index INT,
+title VARCHAR(50),
+age_limit INT,
+runtime INT,
+genre VARCHAR(100),
+rating FLOAT,
+votes INT);
+LOAD DATA LOCAL INFILE 'netflix_shows.csv' INTO TABLE netflix_shows
+FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(name,surname,age);"
+(viewer_index, title, age_limit, runtime, genre, rating, votes);"
 
 
