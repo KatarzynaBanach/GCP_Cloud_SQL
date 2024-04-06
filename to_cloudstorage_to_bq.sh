5ed48e7d09be17e10d14e086a6276bcd72405249
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-06 12:25:34 +0200

    Update to_cloudstorage_to_bq.sh

--- to_cloudstorage_to_bq.sh
+++ to_cloudstorage_to_bq.sh
@@ -1,9 +1,11 @@
-CONNECTION_SERVICE_ACCOUNT=$(gcloud sql instances describe names-inst-new --format="value(serviceAccountEmailAddress)")
+INSTANCE_NAME=netflix-sql
+CONNECTION_SERVICE_ACCOUNT=$(gcloud sql instances describe $INSTANCE_NAME --format="value(serviceAccountEmailAddress)")
 PROJECT_ID=$(gcloud info --format='value(config.project)')
-BUCKET_NAME='myuniquebucketname'
-FILE_NAME='names.csv'
-DATASET_ID='names_dataset'
-TABLE_NAME='names'
+BUCKET_NAME="netflix-$PROJECT_ID"  # Bucket name need to be globally unique. Using Project_ID assures meeting that requirement.
+# using "" instead of '' allows you to use a variable inside. 
+FILE_NAME='netflix_shows.csv'
+DATASET_ID='netflix_from_CS'
+TABLE_NAME='netflix_shows'
 
 # Add the Storage Object Admin role to a service account relevant to Cloud SQL Instance.
 gcloud projects add-iam-policy-binding $PROJECT_ID \
@@ -15,17 +17,17 @@ gcloud storage buckets create gs://$BUCKET_NAME --location=europe-central2
 
 
 # Export data into bucket.
-gcloud sql export csv names-inst-new gs://$BUCKET_NAME/$FILE_NAME \
---database=bts \
+gcloud sql export csv $INSTANCE_NAME gs://$BUCKET_NAME/$FILE_NAME \
+--database=netflix \
 --offload \
---query='SELECT * FROM names' \
+--query='SELECT * FROM netflix_shows' \
 --fields-terminated-by="2C" \
 --lines-terminated-by="0A"
 
 # Create Dataset.
 bq --location='EU' mk \
     --dataset \
-    --description="Data from Cloud SQL" \
+    --description="Netflix Data from Cloud SQL" \
     --label=category:prod \
     $PROJECT_ID:$DATASET_ID
 
@@ -33,11 +35,11 @@ bq --location='EU' mk \
 bq mk \
 --table \
 $PROJECT_ID:$DATASET_ID.$TABLE_NAME \
-names_schema.json
+netflix_shows_schema.json
 
 # Load data into table.
 bq load \
 --source_format=CSV \
 $DATASET_ID.$TABLE_NAME \
 gs://$BUCKET_NAME/$FILE_NAME \
-names_schema.json
+netflix_shows_schema.json
